eb3dff09e49a7b90d8a34e3c94a2d03552476f34
AKninjas <rktaakash@gmail.com>
2023-01-16 15:30:14 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="notfound">
+            <div className="not found-404">
+                <h1>404</h1>
+                <h1>😥</h1>
+            </div>
+            
+        <h2>Page not found</h2>
+            <p className="mb=5">
+            We're sorry,the page you requested could not found any trips.Please go back to the homepage.
+            </p>
+            <br/>
+            <button className="not found">Read More</button>
+        </div>
   );
 }
 
